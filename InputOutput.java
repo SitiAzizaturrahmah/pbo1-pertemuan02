339d1f750840dc2fa0e65496f89dddde65ca492a
siti azizaturrahmah <sitiazizaturrahmah@gmail.com>
2024-04-19 15:46:06 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,13 +1,10 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
-
   String nama;
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
-
- }
+  System.out.println("Hai, "+nama);\
 }
